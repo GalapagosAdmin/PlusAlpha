47c5329c52f4d6c1dc57a19fff2e835c5a7d096b
Noah Silva <shiruba@galapagossoftware.com>
2013-03-17 02:05:13 +0900

    Dynamic database location
    Converted the database unit to automatically look for the database in
    the config directory suggested by LCL.  (Eliminates per system/user
    hard-coding). Also created a script to copy the database from the
    Git/Development directory to the proper directory on OS X.

---
+++ dbRefresh_OSX.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Copies the development version of the Database to the location where the program actually looks for it.
+mkdir ~/.config/PlusAlpha
+cp ./plusalpha.sqlite ~/.config/PlusAlpha/
--- padatabase.pas
+++ padatabase.pas
@@ -8,21 +8,33 @@ uses
   Classes, SysUtils,  sqldb, sqlite3conn;
 
 var
+        DatabaseDir:UTF8String;
+        DatabaseName:UTF8String;
+        CompleteDBPath:UTF8String;
         // Connection to be shared among other objects
         SQLite3Connection1:TSQLite3Connection;
         SQLTransaction1:TSQLTransaction;
 
 implementation
- //uses
 
+uses
+  FileUtil;
 
 initialization
-  SQLite3Connection1 := TSQLite3Connection.Create(nil);
-  SQLite3Connection1.DatabaseName :=  '/Users/shiruba/Develop/plusalpha/plusalpha.sqlite';
-  SQLite3Connection1.Connected := True;
-  SQLTransaction1 := TSQLTransaction.Create(nil);
-  SQLTransaction1.DataBase := SQLite3Connection1;
 
+  DatabaseName := 'plusalpha.sqlite';
+  SQLite3Connection1 := TSQLite3Connection.Create(nil);
+  // The following returns ~/.config/PlusAlpha/ on OS X
+  DatabaseDir := GetAppConfigDirUTF8(False);
+  CompleteDBPath := DatabaseDir + DatabaseName;
+  SQLite3Connection1.DatabaseName := CompleteDBPath;
+  try
+    SQLite3Connection1.Connected := True;
+    SQLTransaction1 := TSQLTransaction.Create(nil);
+    SQLTransaction1.DataBase := SQLite3Connection1;
+  except
+    raise Exception.Create('Database not found at: ' + CompleteDBPath);
+  end;
 
 end.
 
