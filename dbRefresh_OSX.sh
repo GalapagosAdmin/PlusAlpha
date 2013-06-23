d7a18dd1265bc489a2147250037496dc779baed8
GalapagosAdmin <shiruba@yamanotestyle.com>
2013-06-23 15:51:36 +0900

    Updated database refresh script
    updated script which copies the sample database from the GIT repository
    to the local directory to create the ~/.config directory as well.

--- dbRefresh_OSX.sh
+++ dbRefresh_OSX.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # Copies the development version of the Database to the location where the program actually looks for it.
+mkdir /Users/shiruba/.config
 mkdir ~/.config/PlusAlpha
 cp ./plusalpha.sqlite ~/.config/PlusAlpha/
